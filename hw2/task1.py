af48091dafde92f45356ed43a0b597fc9541f717
veto4ka24 <zhbanova.es@phystech.edu>
2022-09-19 20:01:26 +0300

    Update task1.py

--- hw2/task1.py
+++ hw2/task1.py
@@ -1,7 +1,26 @@
+import math as mth
+
+def expon(x, y):
+    r = (x**2 + y**2) ** 0.5
+    phi = mth.atan(y/x)
+    print(r, '*exp(i*', phi, ')')
+
+#def classic(r, phi):
+
+
 class Complex_num:
-
-    def __init__(self, x, y):
-
+    _x = 0
+    _y = 0
+    def __init__(self, x=0, y=0):
+        self.set(x, y)
 
     def get(self):
         return self._x, self._y
+
+    def set(self, x, y):
+        self._x = x
+        self._y = y
+
+z = Complex_num(2, 4)
+print(z.get())
+print(expon(*z.get()))
