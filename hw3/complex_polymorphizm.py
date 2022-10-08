ecf9ad5b93497803a3c0aad823dc506d0acc3c3e
veto4ka24 <zhbanova.es@phystech.edu>
2022-10-08 18:41:44 +0300

    Create complex_polymorphizm.py

---
+++ hw3/complex_polymorphizm.py
@@ -0,0 +1,45 @@
+import math as mth
+
+def sum(z1, z2):
+    u = z1._x + z2._x
+    v = z1._y + z2._y
+    return u, v
+
+def raz(z1, z2):
+    u = z1._x - z2._x
+    v = z1._y - z2._y
+    return u, v
+
+def chast(z1, z2):
+    Re_chisl = z1._x*z2._x - z1._y*z2._y
+    Im_chisl = z1._x*z2._y + z2._x*z1._y
+    znam = (z2._x)**2 + (z2._y)**2
+    u = Re_chisl/znam
+    v = Im_chisl/znam
+    return u, v
+
+def pr(z1, z2):
+    u = z1._x*z2._x - z1._y*z2._y
+    v = z1._y*z2._x + z1._x*z2._y
+    return u, v
+
+class Complex_num:
+    _x = 0
+    _y = 0
+    def __init__(self, x=0, y=0):
+        self.set(x, y)
+
+    def get(self):
+        return self._x, self._y
+
+    def set(self, x, y):
+        self._x = x
+        self._y = y
+
+    def expon(self, x, y):
+        r = (x ** 2 + y ** 2) ** 0.5
+        phi = mth.atan(y / x)
+        print(r, '*exp(i*', phi, ')', sep = '')
+
+    def classic(self, x, y):
+            print(x, ' + i*', y, sep = '')
