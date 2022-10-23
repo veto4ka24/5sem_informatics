43d2dc76f388371ad9be9d1a6e5ad77006618802
veto4ka24 <zhbanova.es@phystech.edu>
2022-10-23 15:50:27 +0300

    Update figures.py

--- hw4/figures.py
+++ hw4/figures.py
@@ -46,10 +46,10 @@ class triangle(figure, point):
         return 'Площадь треугольника S = ' + str(S)
 
     def perimeter(self):
-        P = dist(self._p1, self._p2) + dist(self._p2, self._p3) + dist(p1, p3)
+        P = dist(self._point_1, self._point_2) + dist(self._point_2, self._point_3) + dist(self._point_1, self._point_3)
         return 'Периметр треугольника P = ' + str(P)
 
-class parallelogram(figure):
+class parallelogram(figure, point):
     def __init__(self, p1, p2, p3, p4, type="parallelogram"):
         super().__init__(type)
         self._point_1 = p1
@@ -69,11 +69,73 @@ class parallelogram(figure):
         return 'Площадь параллелограмма S = ' + str(S)
 
     def perimeter(self):
-        P = 2*(dist(p1, p2) + dist(p2, p3))
+        P = 2*(dist(self._point_1, self._point_2) + dist(self._point_2, self._point_3))
+        return 'Периметр параллелограмма P = ' + str(P)
+
+class rhombus(parallelogram):
+    def __init__(self, p1, p2, p3, p4, type="rhombus"):
+        super().__init__(p1, p2, p3, p4, type)
+        self._point_1 = p1
+        self._point_2 = p2
+        self._point_3 = p3
+        self._point_4 = p4
+
+    def area(self):
+        S = 0.5*dist(self._point_1, self._point_3)*dist(self._point_2, self._point_4)
+        return 'Площадь ромба S = ' + str(S)
+
+    def perimeter(self):
+        P = 4*dist(self._point_1, self._point_2)
+        return 'Периметр ромба P = ' + str(P)
+
+class rectangle(parallelogram):
+    def __init__(self, p1, p2, p3, p4, type="rectangle"):
+        super().__init__(p1, p2, p3, p4, type)
+        self._point_1 = p1
+        self._point_2 = p2
+        self._point_3 = p3
+        self._point_4 = p4
+
+    def area(self):
+        S = dist(self._point_1, self._point_2)*dist(self._point_2, self._point_3)
+        return 'Площадь прямоугольника S = ' + str(S)
+
+    def perimeter(self):
+        P = 2 * (dist(self._point_1, self._point_2) + dist(self._point_2, self._point_3))
+        return 'Периметр прямоугольника P = ' + str(P)
+
+class square(rectangle, rhombus):
+    def __init__(self, p1, p2, p3, p4, type="square"):
+        super().__init__(p1, p2, p3, p4, type)
+        self._point_1 = p1
+        self._point_2 = p2
+        self._point_3 = p3
+        self._point_4 = p4
+
+    def area(self):
+        S = dist(self._point_1, self._point_2)**2
+        return 'Площадь квадрата S = ' + str(S)
+
+    def perimeter(self):
+        P = 4*dist(self._point_1, self._point_2)
+        return 'Периметр квадрата P = ' + str(P)
 
 a = triangle(point(), point(1, 3), point(2, 1))
 print(a)
-#print(a.perimeter())
+print(a.perimeter())
 print(a.area())
 b = parallelogram(point(), point(2, 5), point(7, 5), point(5, 0))
 print(b.area())
+print(b.perimeter())
+c = rhombus(point(), point(2, 3), point(0, 4), point(2, -3))
+print(c)
+print(c.area())
+print(c.perimeter())
+d = rectangle(point(), point(0, 3), point(4, 3), point(4, 0))
+print(d)
+print(d.area())
+print(d.perimeter())
+e = square(point(), point(0, 3), point(3, 3), point(3, 0))
+print(e)
+print(e.area())
+print(e.perimeter())
